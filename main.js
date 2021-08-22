b6d932793a505e387210e5794f5a89c338f8bd0b
cal1log <cal1log2527@gmail.com>
2021-08-22 16:05:24 -0500

    delete btoa

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let headers = new Headers();
 let username = 'WY9ZNW2DSS-QXQ_DYT3A-G';
 let password = 'yRSwLDkBR_-1wUl7saZfpg';
-headers.set('Authorization', 'Basic ' + btoa(username + ":" + password));
+headers.set('Authorization', 'Basic ' + username + ":" + password);
 
 async function getMarioPrice() {
     let response = await fetch('https://api.fastspring.com/products/price/mario-kart?country=CO', {headers: headers});    
