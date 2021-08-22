c5681c9b44cf132797ca3c7395e04c7d451ac9b9
cal1log <cal1log2527@gmail.com>
2021-08-22 14:41:48 -0500

    access control allow origin *

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 var headers = new Headers();
-headers.append('Access-Control-Allow-Origin: *', 'Authorization', 'Basic' + btoa('WY9ZNW2DSS-QXQ_DYT3A-G' + ':' + 'iF4838FKQYeFCilMBsQZTQ'));
+headers.append('username', 'WY9ZNW2DSS-QXQ_DYT3A-G');
+headers.append('password', 'iF4838FKQYeFCilMBsQZTQ');
+headers.append('Access-Control-Allow-Origin', '*');
 async function getMarioPrice() {
     let response = await fetch('https://api.fastspring.com/products', {headers: headers});    
     let data = await response.json();
