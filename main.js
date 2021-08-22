52297cc9718987d4ded6fa8855a223b9ecdc62ff
cal1log <cal1log2527@gmail.com>
2021-08-22 14:51:44 -0500

    option headers and let

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
-var headers = new Headers();
+let headers = new Headers();
 headers.append('username', 'WY9ZNW2DSS-QXQ_DYT3A-G');
 headers.append('password', 'iF4838FKQYeFCilMBsQZTQ');
-headers.append('Access-Control-Allow-Origin', '*');
+headers.append('Access-Control-Allow-Origin', 'https://api.fastspring.com/');
+headers.append('Access-Control-Allow-Headers', 'GET');
+headers.append('Connection', 'Keep-Alive');
+headers.append('Content-Type', 'application/json');
+
 async function getMarioPrice() {
-    let response = await fetch('https://api.fastspring.com/products', {headers: headers});    
+    let response = await fetch('https://api.fastspring.com/products/price/mario-kart?country=CO', {headers: headers});    
     let data = await response.json();
     return data;
 }
