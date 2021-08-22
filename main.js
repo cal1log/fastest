0608794dd818fb0ef658274a63a05495d4df4db3
cal1log <cal1log2527@gmail.com>
2021-08-22 11:15:48 -0500

    xmlhttprequest

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
-import { get } from 'axios';
-
-function getPriceMarioKark()
-{
-    get('https://WY9ZNW2DSS-QXQ_DYT3A-G:aRBZZpmtTAarq7igPAgTtQ@api.fastspring.com/products/price/mario-kart?country=CO')
-    .then(function (response) {
-        console.log(response);
-    })
-    .catch(function (error) {
-        console.log(error);
-    })
-    .then(function () {
-    });
+let request = new XMLHttpRequest();
+request.open("GET", "https://WY9ZNW2DSS-QXQ_DYT3A-G:aRBZZpmtTAarq7igPAgTtQ@api.fastspring.com/products/price/mario-kart?country=CO");
+request.send();
+request.onload = () => {
+    if (request.status == 200) {
+        console.log(JSON.parse(request.response));
+    }
+    else {
+        console.log(request.statusText);
+    }
 }
