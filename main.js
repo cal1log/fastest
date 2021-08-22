5035aaac8bfdcd5f030bd2664e5acf12dc61fb2e
cal1log <cal1log2527@gmail.com>
2021-08-22 15:13:35 -0500

    login

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 let headers = new Headers();
-headers.append('username', 'WY9ZNW2DSS-QXQ_DYT3A-G');
-headers.append('password', 'iF4838FKQYeFCilMBsQZTQ');
-headers.append('Access-Control-Allow-Origin', 'https://api.fastspring.com/');
-headers.append('Access-Control-Allow-Headers', 'GET');
-headers.append('Connection', 'Keep-Alive');
+headers.append("username", "WY9ZNW2DSS-QXQ_DYT3A-G");
+headers.append("password", "iF4838FKQYeFCilMBsQZTQ");
+headers.append("Authorization", "Basic " + "WY9ZNW2DSS-QXQ_DYT3A-G" + "iF4838FKQYeFCilMBsQZTQ");
+headers.append("Access-Control-Allow-Origin", "*");
+headers.append("Access-Control-Allow-Headers", "GET, HEAD, OPTIONS, POST, PUT");  
 headers.append('Content-Type', 'application/json');
 
 async function getMarioPrice() {
