35e403a815885267cbed2b9375e0dae3705573cb
cal1log <cal1log2527@gmail.com>
2021-08-22 14:12:09 -0500

    url

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var headers = new Headers();
-headers.append('Authorization', 'Basic' + btoa('WY9ZNW2DSS-QXQ_DYT3A-G' + ':' + 'aRBZZpmtTAarq7igPAgTtQ'));
+headers.append('Authorization', 'Basic' + btoa('WY9ZNW2DSS-QXQ_DYT3A-G' + ':' + 'iF4838FKQYeFCilMBsQZTQ'));
 async function getMarioPrice() {
-    let response = await fetch('https://api.fastspring.com/products/price/mario-kart?country=CO', {
+    let response = await fetch('https://api.fastspring.com/products', {
         'mode': 'no-cors',
         'headers': {
             'Access-Control-Allow-Origin': '*',
