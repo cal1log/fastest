ed1b404470b05fe409db9934fd408256f783ea3f
cal1log <cal1log2527@gmail.com>
2021-08-22 15:47:32 -0500

    auth

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let headers = new Headers();
 headers.append("Authorization", "Basic " + "WY9ZNW2DSS-QXQ_DYT3A-G" + "iF4838FKQYeFCilMBsQZTQ");
-headers.append("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+headers.append("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
 async function getMarioPrice() {
     let response = await fetch('https://api.fastspring.com/products/price/mario-kart?country=CO', {headers: headers});    
