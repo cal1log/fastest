f5f03285516854a65698987a650116c14e664413
cal1log <cal1log2527@gmail.com>
2021-08-22 16:00:11 -0500

    header set auth

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let headers = new Headers();
-headers.append("Authorization", "Basic " + "WY9ZNW2DSS-QXQ_DYT3A-G" + "iF4838FKQYeFCilMBsQZTQ");
-headers.append("Access-Control-Allow-Headers", "Content-Type, Authorization");
+let username = 'WY9ZNW2DSS-QXQ_DYT3A-G';
+let password = 'yRSwLDkBR_-1wUl7saZfpg';
+headers.set('Authorization', 'Basic ' + btoa(username + ":" + password));
 
 async function getMarioPrice() {
     let response = await fetch('https://api.fastspring.com/products/price/mario-kart?country=CO', {headers: headers});    
