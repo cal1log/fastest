299e964e1d747580a8c84201d24c9479b9458878
cal1log <cal1log2527@gmail.com>
2021-08-22 10:57:46 -0500

    delete require@

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-const { default: axios } = require("axios");
-
 function getPriceMarioKark()
 {
     axios.get('https://WY9ZNW2DSS-QXQ_DYT3A-G:aRBZZpmtTAarq7igPAgTtQ@api.fastspring.com/products/price/mario-kart?country=CO')
