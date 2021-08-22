c0e0bc6e3496238ebc87a57cb5687f67d7354dd5
cal1log <cal1log2527@gmail.com>
2021-08-22 17:33:34 -0500

    no cors 2

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ let username = 'WY9ZNW2DSS-QXQ_DYT3A-G';
 let password = '8-K4f-tpSf2kBWFROH34tA';
 let headers = new Headers();
 headers.set('Authorization', 'Basic ' + btoa(username + ":" + password));
-headers.set('User-Agent', 'Chrome');
+headers.append('User-Agent', 'Chrome');
 
 async function getMarioPrice() {
-    let response = await fetch(url, {mode: 'no-cors', headers: headers});    
+    let response = await fetch(url, {headers: headers});    
     let data = await response.json();
     return data;
 }
