7e3c249adcbcc401bc937288d0d877475f1a30c1
cal1log <cal1log2527@gmail.com>
2021-08-22 18:49:13 -0500

    set

--- main.js
+++ main.js
@@ -2,9 +2,8 @@ let url = 'https://api.fastspring.com/products/price/mario-kart?country=CO';
 let username = 'WY9ZNW2DSS-QXQ_DYT3A-G';
 let password = '8-K4f-tpSf2kBWFROH34tA';
 let headers = new Headers();
-headers.append('Authorization', 'Basic ' + window.btoa(username + ":" + password));
+headers.set('Authorization', 'Basic ' + window.btoa(username + ":" + password));
 headers.set('User-Agent', 'Chrome/77.0.3865.90');
-headers.set('Access-Control-Allow-Origin', '*');
 
 async function getMarioPrice() {
     let response = await fetch(url, {headers: headers, credentials: 'same-origin'});    
