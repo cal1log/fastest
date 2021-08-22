a9fd328a7ae782bebc5ba8ab85fcee2675544788
cal1log <cal1log2527@gmail.com>
2021-08-22 14:18:04 -0500

    password

--- main.js
+++ main.js
@@ -1,13 +1,7 @@
 var headers = new Headers();
 headers.append('Authorization', 'Basic' + btoa('WY9ZNW2DSS-QXQ_DYT3A-G' + ':' + 'iF4838FKQYeFCilMBsQZTQ'));
 async function getMarioPrice() {
-    let response = await fetch('https://api.fastspring.com/products', {
-        'mode': 'no-cors',
-        'headers': {
-            'Access-Control-Allow-Origin': '*',
-            headers,
-        }    
-    });
+    let response = await fetch('https://api.fastspring.com/products', {headers: headers});    
     let data = await response.json();
     return data;
 }
