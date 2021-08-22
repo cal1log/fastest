5edc0f7e8669cd60a31fe6da2fec70390665811c
cal1log <cal1log2527@gmail.com>
2021-08-22 10:49:27 -0500

    api key

---
+++ main.js
@@ -0,0 +1,14 @@
+const { default: axios } = require("axios");
+
+function getPriceMarioKark()
+{
+    axios.get('https://WY9ZNW2DSS-QXQ_DYT3A-G:aRBZZpmtTAarq7igPAgTtQ@api.fastspring.com/products/price/mario-kart?country=CO')
+    .then(function (response) {
+        console.log(response);
+    })
+    .catch(function (error) {
+        console.log(error);
+    })
+    .then(function () {
+    });
+}
