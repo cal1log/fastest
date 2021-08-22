d5981e91c889d700ac5c509d06b85ecb2125594d
cal1log <cal1log2527@gmail.com>
2021-08-22 11:46:48 -0500

    fetch

--- main.js
+++ main.js
@@ -1,11 +1,7 @@
-let request = new XMLHttpRequest();
-request.open("GET", "https://WY9ZNW2DSS-QXQ_DYT3A-G:aRBZZpmtTAarq7igPAgTtQ@api.fastspring.com/products/price/mario-kart?country=CO");
-request.send();
-request.onload = () => {
-    if (request.status == 200) {
-        console.log(JSON.parse(request.response));
-    }
-    else {
-        console.log(request.statusText);
-    }
+async function getMarioPrice() {
+    let response = await fetch('https://WY9ZNW2DSS-QXQ_DYT3A-G:aRBZZpmtTAarq7igPAgTtQ@api.fastspring.com/products/price/mario-kart?country=CO');
+    let data = await response.json();
+    return data;
 }
+
+getMarioPrice().then(data => console.log(data));
