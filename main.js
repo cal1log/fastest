6d69675e845ba61fbc27a0135a9366947f9f6e70
cal1log <cal1log2527@gmail.com>
2021-08-22 20:58:01 -0500

    delete mode

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
 async function getMario() {
     try {
-        let response = await fetch('https://api.fastspring.com/products', {
+        let response = await fetch('https://api.fastspring.com/products/mario-kart', {
             method: 'GET',
             headers: {
-                'Authorization': 'Basic ' + window.btoa('WY9ZNW2DSS-QXQ_DYT3A-G' + ":" + 'y-Qv9AioT6O31zqXOddSOA'),
-                'Content-Type': 'application/json',
+                'Authorization': 'Basic ' + window.btoa('WY9ZNW2DSS-QXQ_DYT3A-G' + ":" + 'y-Qv9AioT6O31zqXOddSOA'),                
                 'User-Agent': 'Chrome/92.0.4515.159'        
-            },
-            mode: 'no-cors',
+            },            
             cache: 'default',
         });
         let data = await response.json();
