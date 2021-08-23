5df41a9e548a2d7b0872d23f19c3175eea8e346f
cal1log <cal1log2527@gmail.com>
2021-08-22 21:02:50 -0500

    no cors

--- main.js
+++ main.js
@@ -5,7 +5,8 @@ async function getMario() {
             headers: {
                 'Authorization': 'Basic ' + window.btoa('WY9ZNW2DSS-QXQ_DYT3A-G' + ":" + 'y-Qv9AioT6O31zqXOddSOA'),                
                 'User-Agent': 'Chrome/92.0.4515.159'        
-            },            
+            },
+            mode: 'no-cors',            
             cache: 'default',
         });
         let data = await response.json();
