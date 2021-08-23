167ab4979f83cac200aed06f7e446b7555ef39a2
cal1log <cal1log2527@gmail.com>
2021-08-22 20:04:27 -0500

    node

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-let url = 'https://api.fastspring.com/products/price/mario-kart?country=CO';
-let username = 'WY9ZNW2DSS-QXQ_DYT3A-G';
-let password = '8-K4f-tpSf2kBWFROH34tA';
+const https = require('https');
 
-async function getMarioPrice() {
-    let response = await fetch(url, {
-        headers: { 
-            'Authorization': 'Basic ' + btoa(username + ":" + password)
-        }
-    });    
-    let data = await response.json();
-    return data;
-}
+https.get('https://api.fastspring.com/products/price/mario-kart?country=CO', (response) => {
+    let data = '';
+    response.on('data', (chunk) => {
+        data += chunk;
+    });
 
-getMarioPrice().then(data => console.log(data));
+    response.on('end', () => {
+        console.log(data);
+    });
+})
+.on('error', (error) => {
+    console.log(error);
+});
