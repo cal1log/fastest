24de9d61daf2e623c27db2a71ca63017b8c08e55
cal1log <cal1log2527@gmail.com>
2021-08-22 19:33:40 -0500

    delete headers

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 let url = 'https://api.fastspring.com/products/price/mario-kart?country=CO';
 let username = 'WY9ZNW2DSS-QXQ_DYT3A-G';
 let password = '8-K4f-tpSf2kBWFROH34tA';
-let headers = new Headers();
-headers.set('Authorization', 'Basic ' + btoa(username + ":" + password));
 
 async function getMarioPrice() {
-    let response = await fetch(url, {headers: headers});    
+    let response = await fetch(url, {
+        'Authorization': 'Basic ' + btoa(username + ":" + password); 
+    });    
     let data = await response.json();
     return data;
 }
