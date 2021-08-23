a29ef2ddb8b722bf352d7606ae9e0b1b38dc1d1b
cal1log <cal1log2527@gmail.com>
2021-08-23 08:23:24 -0500

    telnet

--- main.js
+++ main.js
@@ -4,12 +4,13 @@ async function getMario() {
             method:'GET',
             headers: {
                 'Authorization': 'Basic ' + window.btoa('WY9ZNW2DSS-QXQ_DYT3A-G' + ":" + 'y-Qv9AioT6O31zqXOddSOA'),                
-                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_5_2) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.2 Safari/605.1.15'        
+                'User-Agent': 'telnet'        
             },
             mode: 'no-cors',            
             cache: 'default',
         });
         let data = await response.json();
+        console.log(data);
         return data;
     } catch (err) {
         return err;
