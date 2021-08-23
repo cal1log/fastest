78e87f872da732c21a3f5a59e6e2e587682b252f
cal1log <cal1log2527@gmail.com>
2021-08-22 20:32:57 -0500

    fetch

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
-const https = require('https');
-
-https.get('https://api.fastspring.com/products/price/mario-kart?country=CO', (response) => {
-    let data = '';
-    response.on('data', (chunk) => {
-        data += chunk;
-    });
-
-    response.on('end', () => {
-        console.log(data);
-    });
-})
-.on('error', (error) => {
-    console.log(error);
-});
+async function getMario() {
+    try {
+        let response = await fetch('https://api.fastspring.com/products', {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Basic ' + window.btoa('WY9ZNW2DSS-QXQ_DYT3A-G' + ":" + 'y-Qv9AioT6O31zqXOddSOA'),
+                'Content-Type': 'application/json'        
+            },
+            mode: 'no-cors',
+            cache: 'default',
+        });
+        let data = await response.json();
+        return data;
+    } catch (err) {
+        return err;
+    }
+}
