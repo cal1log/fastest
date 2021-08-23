e46b94de36c818ce2a74dd8d12ad274f4e01e63d
cal1log <cal1log2527@gmail.com>
2021-08-23 13:05:10 -0500

    delete keys

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ async function getMario() {
         let response = await fetch('https://api.fastspring.com/products/mario-kart', {
             method:'GET',
             headers: {
-                'Authorization': 'Basic ' + window.btoa('WY9ZNW2DSS-QXQ_DYT3A-G' + ":" + 'y-Qv9AioT6O31zqXOddSOA'),                
+                                
                 'User-Agent': window.navigator.userAgent        
             },
             mode: 'no-cors'                        
