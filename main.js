730d8226021542cb8940608b98728f98b1482a93
cal1log <cal1log2527@gmail.com>
2021-08-22 21:22:20 -0500

    no cors

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 async function getMario() {
     try {
         let response = await fetch('https://api.fastspring.com/products/mario-kart', {
-            method: 'GET',
+            method:'GET',
             headers: {
                 'Authorization': 'Basic ' + window.btoa('WY9ZNW2DSS-QXQ_DYT3A-G' + ":" + 'y-Qv9AioT6O31zqXOddSOA'),                
-                'User-Agent': 'Chrome/92.0.4515.159'        
+                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_5_2) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.2 Safari/605.1.15'        
             },
             mode: 'no-cors',            
             cache: 'default',
