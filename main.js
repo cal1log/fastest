4c555d7b5a5553d620de921c7dc376822d06661e
cal1log <cal1log2527@gmail.com>
2021-08-22 19:24:28 -0500

    delete credentials

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ let url = 'https://api.fastspring.com/products/price/mario-kart?country=CO';
 let username = 'WY9ZNW2DSS-QXQ_DYT3A-G';
 let password = '8-K4f-tpSf2kBWFROH34tA';
 let headers = new Headers();
-headers.set('Authorization', 'Basic ' + window.btoa(username + ":" + password));
+headers.set('Authorization', 'Basic ' + btoa(username + ":" + password));
 
 async function getMarioPrice() {
-    let response = await fetch(url, {headers: headers, credentials: 'same-origin'});    
+    let response = await fetch(url, {headers: headers});    
     let data = await response.json();
     return data;
 }
